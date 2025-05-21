7db03e492e09bdee282bdf2a555b89bbc90c989a
MonkeysWithPie <128157282+MonkeysWithPie@users.noreply.github.com>
2025-05-21 09:49:21 -0500

    half-baked calculation because tokenization is hard

--- calculate.js
+++ calculate.js
@@ -1,23 +1,96 @@
 const { getMessage } = require("./messages");
 const { printMessage } = require("./printMessage");
 
+const operators = {
+    "+": { 
+        func: (a, b) => a + b,
+        precidence: 1,
+        text: "plus",
+    },
+    "-": { 
+        func: (a, b) => a - b,
+        precidence: 1,
+        text: "minus",
+    },
+    "*": { 
+        func: (a, b) => a * b,
+        precidence: 2,
+        text: "times",
+    },
+    "/": { 
+        func: (a, b) => a / b,
+        precidence: 2,
+        text: "divided by",
+    },
+    "^": { 
+        func: (a, b) => Math.pow(a, b),
+        precidence: 3,
+        text: "to the power of",
+    },
+};
+
 async function calculate(equation) {
     if (Math.random() < 0.7) {
         printMessage(getMessage("distracted"));
         return null;
-    }
+    }   
+    let result;
 
-    const operators = {
-        "+": (a, b) => a + b,
-        "-": (a, b) => a - b,
-        "*": (a, b) => a * b,
-        "/": (a, b) => a / b,
-        "^": (a, b) => Math.pow(a, b),
-    };
+    try {
+        // do all the calculation
+    } catch (e) {
+        printMessage(getMessage("error"));
+        printMessage(`(${e}, ${e.stack})`, 15);
+        return null;
+    }
 
     // give up because this seems really complicated to implement
     printMessage("idk... like... four?");
     return 4;
+}
+
+
+function tokenize(equation) {
+    const tokens = [];
+    let currentNumber = "";
+
+    for (let i = 0; i < equation.length; i++) {
+        const char = equation[i];
+
+        if (char === " ") {
+            continue;
+        }
+        if (parseInt(char) >= 0 && parseInt(char) <= 9) {
+            currentNumber += char;
+            continue;
+        }
+        if (currentNumber) {
+            tokens.push(parseInt(currentNumber));
+            currentNumber = "";
+        }
+
+        tokens.push(char);
+    }
+
+    return tokens;
+}
+
+function parseTokens(tokens) {
+
+}
+
+async function calculate(a, b, op, calculations) {
+    if (!operators[op]) {
+        throw new Error(`no operator for ${op}`);
+    }
+
+    const result = operators[op].func(a, b);
+
+    await printMessage(
+        operators[op].text + " " + b + " " + getMessage("result") + " " + result + "...",
+        50
+    )
+    return result;
 }
 
 module.exports = { calculate };
